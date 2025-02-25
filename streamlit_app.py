2a3e86d9f5696f94075f54f2bd73fb0839564a24
Rahmat Ramadhani <47129927+rramadhani015@users.noreply.github.com>
2025-02-25 18:20:34 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -11,10 +11,8 @@ bearing = st.sidebar.slider("Bearing", min_value=0, max_value=360, value=0, step
 
 # Define the viewport for the map
 view = pdk.ViewState(
-    # latitude=32.7213,  # Example location
-    # longitude=-114.26608,
-    latitude=854,  # Example location
-    longitude=396,
+    latitude=32.7213,  # Example location
+    longitude=-114.26608,
     zoom=12,
     pitch=pitch,
     bearing=bearing
@@ -26,8 +24,8 @@ terrain_layer = pdk.Layer(
     data=None,  # No input data required for raster layers
     elevation_decoder={"rScaler": 256, "gScaler": 1, "bScaler": 1 / 256, "offset": -32768},
     # elevation_data="mapbox://mapbox.mapbox-terrain-dem-v1",
-    # elevation_data="https://api.mapbox.com/v4/mapbox.terrain-rgb/{zoom}/{x}/{y}{@2x}.pngraw?access_token="+ MAPBOX_API_KEY,
-    elevation_data="https://api.mapbox.com/v4/mapbox.terrain-rgb/10/854/396.pngraw?access_token=pk.eyJ1IjoicmFtYWRoYW5pMDE1IiwiYSI6ImNtN2p6N21oaDBhaDcyanMzMHRiNjJsOTEifQ.tS3O3ERXLBjrqlfYep2OLQ",
+    elevation_data="https://api.mapbox.com/v4/mapbox.terrain-rgb/{zoom}/{x}/{y}{@2x}.pngraw?access_token="+ MAPBOX_API_KEY,
+    # elevation_data="https://api.mapbox.com/v4/mapbox.terrain-rgb/10/854/396.pngraw?access_token=pk.eyJ1IjoicmFtYWRoYW5pMDE1IiwiYSI6ImNtN2p6N21oaDBhaDcyanMzMHRiNjJsOTEifQ.tS3O3ERXLBjrqlfYep2OLQ",
     # https://api.mapbox.com/v4/mapbox.terrain-rgb/10/854/396.pngraw?access_token=pk.eyJ1IjoicmFtYWRoYW5pMDE1IiwiYSI6ImNtN2p6N21oaDBhaDcyanMzMHRiNjJsOTEifQ.tS3O3ERXLBjrqlfYep2OLQ
     texture="https://api.mapbox.com/styles/v1/mapbox/satellite-v9/tiles/256/{z}/{x}/{y}?access_token=" + MAPBOX_API_KEY,
     exaggeration=1.5,
